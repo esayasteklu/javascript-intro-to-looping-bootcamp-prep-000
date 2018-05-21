9cce2a78adc18c45f1a924040c4cce5f25c8c562
Esayas Gebre <dave183278@gmail.com>
2018-05-21 20:43:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,9 +11,9 @@ function forLoop(array)
   for ( i=0 ; i < l ;i++)
   {
     if (i===1)
-    array[i]= "I am 1 strange loop."
+    array[i].push= "I am 1 strange loop."
     else
-    array[i] =`I am ${i} strange loops.`
+    array[i].push =`I am ${i} strange loops.`
     
    
   }
