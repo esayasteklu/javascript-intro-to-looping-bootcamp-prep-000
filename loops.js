2618f2e14825a0c1a8a075196e82831a46de2820
Esayas Gebre <dave183278@gmail.com>
2018-05-21 20:52:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -41,9 +41,12 @@ function maybeTrue()
   return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
 
+function ()
+{
 do
 {
   
   
 } while( array.length > 0 && maybeTrue());
 
+}
